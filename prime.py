9914b666a92e11c551ef89f494d2cded3bd8e9d6
Finnian Anderson <finnian@fxapi.co.uk>
2014-10-26 08:57:47 +0000

    Fix reversed results in isPrime()

--- prime.py
+++ prime.py
@@ -3,5 +3,5 @@ import math
 def isPrime(num): # returns True or False depending on if supplied number is prime
 	if num < 2: return False # number is less than two so it's not prime
 	for i in range(2, int(math.sqrt(num)) + 1): # for every number up until the number's sqrt
-		if num % i == 0: return True # if it's divisible by two
-	return False # otherwise
+		if num % i == 0: return False # if it's divisible by two
+	return True # otherwise
