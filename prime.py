5c345abfa7ba7f41990fd5862c9df3302331e6b6
Finnian Anderson <finnian@fxapi.co.uk>
2014-10-26 12:31:50 +0000

    Update prime.py

--- prime.py
+++ prime.py
@@ -6,16 +6,16 @@ def isPrime(num): # returns True or False depending on if supplied number is pri
 		if num % i == 0: return False # if it's divisible by two
 	return True # otherwise
 
-def primeSieve(sieveSize):
-	sieve = [True] * sieveSize
-	sieve[0] = False
+def primesUpTo(num): # returns list of primes up until num
+	sieve = [True] * num
+	sieve[0] = False # 0 and 1 are not prime
 	sieve[1] = False
-	for i in range(2, int(math.sqrt(sieveSize)) + 1):
+	for i in range(2, int(math.sqrt(num)) + 1): # loop through all numbers up until the sqrt of num
 		pointer = i * 2
-		while pointer < sieveSize:
+		while pointer < num:
 			sieve[pointer] = False
 			pointer += i
 	primes = []
-	for i in range(sieveSize):
-		if sieve[i] == True: primes.append(i)
-	return primes
+	for i in range(num):
+		if sieve[i] == True: primes.append(i) # then it's a prime
+	return primes # let's have the primes!
